7a0a386c5818ef093bb023b9f000c15e76b36d78
Matus Poruben <poruben.matus@gmail.com>
2021-05-02 19:43:16 +0200

    adding config

---
+++ configs/prettier.config.js
@@ -0,0 +1,9 @@
+// prettier.config.js or .prettierrc.js
+module.exports = {
+    trailingComma: 'es5',
+    printWidth: 80,
+    proseWrap: 'always',
+    // semi: false,
+    singleQuote: true,
+  };
+  
